6b16867bb10002c26324b706dfef32e48036b5e7
Digitalone <kurmikon@libero.it>
2023-02-02 17:04:27 +0100

    rearrange db script migration data for testing

--- src/dev-runner/migrations/0001-initial-migration.sql
+++ src/dev-runner/migrations/0001-initial-migration.sql
@@ -72,12 +72,12 @@ CREATE TABLE versions (
     status versionStatus NOT NULL,
     semver VARCHAR(256) NOT NULL,
     license VARCHAR(128) NOT NULL,
-    created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     engine JSONB NOT NULL,
     repo_type repository NOT NULL DEFAULT 'git',
     repo_url TEXT NOT NULL DEFAULT '',
     readme TEXT NOT NULL DEFAULT '',
+    created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     meta JSONB,
     deleted BOOLEAN NOT NULL DEFAULT FALSE,
     -- generated columns
@@ -111,32 +111,19 @@ CREATE TABLE authstate (
 
 INSERT INTO packages (pointer, package_type, name, creation_method, downloads, stargazers_count, data, original_stargazers)
 VALUES (
-  'd28c7ce5-c9c4-4fb6-a499-a7c6dcec355b', 'package', 'language-css', 'user made', 400004, 1,
-  '{"name": "language-css", "readme": "Cool readme", "metadata": {"bugs": {"url": "https://github.com/atom/language-css/issues"},
-  "name": "language-css", "engines": {"atom": "*","node":"*"},"license":"MIT","version":"0.45.7","homepage":"http://atom.github.io/language-css",
-  "keywords":["tree-sitter"],"repository":{"url":"https://github.com/atom/language-css.git","type":"git"},"description":"CSS Support in Atom",
-  "dependencies":{"tree-sitter-css":"^0.19.0"},"devDependencies":{"coffeelint":"^1.10.1"}},"repository":{"url":"https://github.com/atom/langauge-css",
-  "type":"git"}}', 76
+  'd28c7ce5-c9c4-4fb6-a499-a7c6dcec355b', 'package', 'language-css', 'user made', 400004, 1, '{}', 76
 ), (
-  'd27dbd37-e58e-4e02-b804-9e3e6ae02fb1', 'package', 'language-cpp', 'user made', 849156, 1,
-  '{"name": "language-cpp", "description": "C++ Support in Atom", "keywords": ["tree-sitter"]}', 91
+  'd27dbd37-e58e-4e02-b804-9e3e6ae02fb1', 'package', 'language-cpp', 'user made', 849156, 1, '{}', 91
 ), (
-  'ee87223f-65ab-4a1d-8f45-09fcf8e64423', 'package', 'hydrogen', 'Migrated from Atom.io', 2562844, 1,
-  '{"name": "hydrogen", "readme": "Hydrogen Readme", "metadata": { "main": "./dist/main", "name": "Hydrogen",
-  "author": "nteract contributors", "engines": {"atom": ">=1.28.0 <2.0.0"}, "license": "MIT", "version": "2.16.3"}}', 821
+  'ee87223f-65ab-4a1d-8f45-09fcf8e64423', 'package', 'hydrogen', 'Migrated from Atom.io', 2562844, 1, '{}', 821
 ), (
-  'aea26882-8459-4725-82ad-41bf7aa608c3', 'package', 'atom-clock', 'Migrated from Atom.io', 1090899, 1,
-  '{"name": "atom-clock", "readme": "Atom-clok!", "metadata": { "main": "./lib/atom-clock", "name": "atom-clock",
-  "author": { "url": "https://github.com/b3by", "name": "Antonio Bevilacqua", "email": "b3by.in.the3.sky@gmail.com"}}}', 528
+  'aea26882-8459-4725-82ad-41bf7aa608c3', 'package', 'atom-clock', 'Migrated from Atom.io', 1090899, 1, '{}', 528
 ), (
-  '1e19da12-322a-4b37-99ff-64f866cc0cfa', 'package', 'hey-pane', 'Migrated from Atom.io', 206804, 1,
-  '{"name": "hey-pane", "readme": "hey-pane!", "metadata": { "main": "./lib/hey-pane", "license": "MIT"}}', 176
+  '1e19da12-322a-4b37-99ff-64f866cc0cfa', 'package', 'hey-pane', 'Migrated from Atom.io', 206804, 1, '{}', 176
 ), (
-  'a0ef01cb-720e-4c0d-80c5-f0ed441f31fc', 'theme', 'atom-material-ui', 'Migrated from Atom.io', 2509605, 1,
-  '{"name": "atom-material-ui", "readme": "ATOM!"}', 1772
+  'a0ef01cb-720e-4c0d-80c5-f0ed441f31fc', 'theme', 'atom-material-ui', 'Migrated from Atom.io', 2509605, 1, '{}', 1772
 ), (
-  '28952de5-ddbf-41a8-8d87-5d7e9d7ad7ac', 'theme', 'atom-material-syntax', 'Migrated from Atom.io', 1743927, 1,
-  '{"name": "atom-material-syntax"}', 1309
+  '28952de5-ddbf-41a8-8d87-5d7e9d7ad7ac', 'theme', 'atom-material-syntax', 'Migrated from Atom.io', 1743927, 1, '{}', 1309
 );
 
 INSERT INTO names (name, pointer)
@@ -156,38 +143,67 @@ VALUES (
   'atom-material-syntax', '28952de5-ddbf-41a8-8d87-5d7e9d7ad7ac'
 );
 
-INSERT INTO versions (package, status, semver, license, engine, meta)
+INSERT INTO versions (package, status, semver, license, engine, created, repo_url, readme, meta)
 VALUES (
-  'd28c7ce5-c9c4-4fb6-a499-a7c6dcec355b', 'published', '0.45.7', 'MIT', '{"atom": "*", "node": "*"}',
+  'd28c7ce5-c9c4-4fb6-a499-a7c6dcec355b', 'published', '0.45.7', 'MIT', '{"atom": "*", "node": "*"}', '2023-02-01 12:00:00', 'https://github.com/pulsar-edit/language-css', 'THIS IS A README',
   '{"name": "language-css", "description": "CSS Support in Atom", "keywords": ["tree-sitter"],
-     "tarball_url": "https://github.com/pulsar-edit/language-css"}'
+    "tarball_url": "https://github.com/pulsar-edit/language-css",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  'd28c7ce5-c9c4-4fb6-a499-a7c6dcec355b', 'latest', '0.46.0', 'MIT', '{"atom": "*", "node": "*"}',
-  '{"name": "language-css", "description": "CSS Support in Atom", "keywords": ["tree-sitter"]}'
+  'd28c7ce5-c9c4-4fb6-a499-a7c6dcec355b', 'latest', '0.46.0', 'MIT', '{"atom": "*", "node": "*"}', '2023-02-01 12:01:00', 'https://github.com/pulsar-edit/language-css', 'THIS IS A README',
+  '{"name": "language-css", "description": "CSS Support in Atom", "keywords": ["tree-sitter"],
+    "tarball_url": "https://github.com/pulsar-edit/language-css",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  'd28c7ce5-c9c4-4fb6-a499-a7c6dcec355b', 'published', '0.45.0', 'MIT', '{"atom": "*", "node": "*"}',
-  '{"name":"language-css", "description": "CSS Support in Atom", "keywords": ["tree-sitter"]}'
+  'd28c7ce5-c9c4-4fb6-a499-a7c6dcec355b', 'published', '0.45.0', 'MIT', '{"atom": "*", "node": "*"}', '2023-02-01 12:02:00', 'https://github.com/pulsar-edit/language-css', 'THIS IS A README',
+  '{"name":"language-css", "description": "CSS Support in Atom", "keywords": ["tree-sitter"],
+    "tarball_url": "https://github.com/pulsar-edit/language-css",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  'd27dbd37-e58e-4e02-b804-9e3e6ae02fb1', 'published', '0.11.8', 'MIT', '{"atom": "*", "node": "*"}',
-  '{"name": "language-cpp", "description": "C++ Support in Atom", "keywords": ["tree-sitter"]}'
+  'd27dbd37-e58e-4e02-b804-9e3e6ae02fb1', 'published', '0.11.8', 'MIT', '{"atom": "*", "node": "*"}', '2023-02-01 12:02:00', 'https://github.com/pulsar-edit/language-cpp', 'THIS IS A README',
+  '{"name": "language-cpp", "description": "C++ Support in Atom", "keywords": ["tree-sitter"],
+    "tarball_url": "https://github.com/pulsar-edit/language-cpp",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  'd27dbd37-e58e-4e02-b804-9e3e6ae02fb1', 'latest', '0.11.9', 'MIT', '{"atom": "*", "node": "*"}',
-  '{"name": "language-cpp", "description": "C++ Support in Atom", "keywords": ["tree-sitter"]}'
+  'd27dbd37-e58e-4e02-b804-9e3e6ae02fb1', 'latest', '0.11.9', 'MIT', '{"atom": "*", "node": "*"}', '2023-02-01 12:03:00', 'https://github.com/pulsar-edit/language-cpp', 'THIS IS A README',
+  '{"name": "language-cpp", "description": "C++ Support in Atom", "keywords": ["tree-sitter"],
+    "tarball_url": "https://github.com/pulsar-edit/language-cpp",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  'ee87223f-65ab-4a1d-8f45-09fcf8e64423', 'latest', '2.16.3', 'MIT', '{"atom": "*"}',
-  '{"name": "Hydrogen", "dist": {"tarball": "https://www.atom.io/api/packages/hydrogen/version/2.16.3/tarball"}}'
+  'ee87223f-65ab-4a1d-8f45-09fcf8e64423', 'latest', '2.16.3', 'MIT', '{"atom": "*"}', '2023-02-01 12:04:00',
+  'https://www.atom.io/api/packages/hydrogen/version/2.16.3/tarball', 'THIS IS A README',
+  '{"name": "hydrogen",
+    "dist": {"tarball": "https://www.atom.io/api/packages/hydrogen/version/2.16.3/tarball"},
+    "tarball_url": "https://www.atom.io/api/packages/hydrogen/version/2.16.3/tarball",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  'aea26882-8459-4725-82ad-41bf7aa608c3', 'latest', '0.1.18', 'MIT', '{"atom": "*"}',
-  '{"name": "atom-clock", "dist": {"tarball": "https://www.atom.io/api/packages/atom-clock/version/1.18.0/tarball"}}'
+  'aea26882-8459-4725-82ad-41bf7aa608c3', 'latest', '0.1.18', 'MIT', '{"atom": "*"}', '2023-02-01 12:05:00',
+  'https://www.atom.io/api/packages/atom-clock/version/1.18.0/tarball', 'THIS IS A README',
+  '{"name": "atom-clock",
+    "dist": {"tarball": "https://www.atom.io/api/packages/atom-clock/version/1.18.0/tarball"},
+    "tarball_url": "https://www.atom.io/api/packages/atom-clock/version/1.18.0/tarball",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  '1e19da12-322a-4b37-99ff-64f866cc0cfa', 'latest', '1.2.0', 'MIT', '{"atom": "*"}',
-  '{"name":"hey-pane", "dist": {"tarball": "https://www.atom.io/api/packages/hydrogen/version/1.2.0/tarball"}}'
+  '1e19da12-322a-4b37-99ff-64f866cc0cfa', 'latest', '1.2.0', 'MIT', '{"atom": "*"}', '2023-02-01 12:06:00',
+  'https://www.atom.io/api/packages/hey-pane/version/1.2.0/tarball', 'THIS IS A README',
+  '{"name":"hey-pane",
+    "dist": {"tarball": "https://www.atom.io/api/packages/hey-pane/version/1.2.0/tarball"},
+    "tarball_url": "https://www.atom.io/api/packages/hey-pane/version/1.2.0/tarball",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  'a0ef01cb-720e-4c0d-80c5-f0ed441f31fc', 'latest', '2.1.3', 'MIT', '{"atom": "*"}',
-  '{"name": "atom-material-ui", "dist": {"tarball": "https://www.atom.io/api/packages/atom-material-ui/version/2.1.3/tarball"}}'
+  'a0ef01cb-720e-4c0d-80c5-f0ed441f31fc', 'latest', '2.1.3', 'MIT', '{"atom": "*"}', '2023-02-01 12:07:00',
+  'https://www.atom.io/api/packages/atom-material-ui/version/2.1.3/tarball', 'THIS IS A README',
+  '{"name": "atom-material-ui",
+    "dist": {"tarball": "https://www.atom.io/api/packages/atom-material-ui/version/2.1.3/tarball"},
+    "tarball_url": "https://www.atom.io/api/packages/atom-material-ui/version/2.1.3/tarball",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 ), (
-  '28952de5-ddbf-41a8-8d87-5d7e9d7ad7ac', 'latest', '1.0.8', 'MIT', '{"atom":"*"}',
-  '{"name": "atom-material-syntax", "dist": {"tarball":"https://www.atom/io/api/packages/atom-material-syntax/version/1.0.8/tarball"}}'
+  '28952de5-ddbf-41a8-8d87-5d7e9d7ad7ac', 'latest', '1.0.8', 'MIT', '{"atom":"*"}', '2023-02-01 12:08:00',
+  'https://www.atom/io/api/packages/atom-material-syntax/version/1.0.8/tarball', 'THIS IS A README',
+  '{"name": "atom-material-syntax",
+    "dist": {"tarball":"https://www.atom/io/api/packages/atom-material-syntax/version/1.0.8/tarball"},
+    "tarball_url": "https://www.atom/io/api/packages/atom-material-syntax/version/1.0.8/tarball",
+    "sha": "570CC7A82B4807E15CFC4835A50802FD4BC84456"}'
 );
 
 INSERT INTO users (username, node_id, avatar)
